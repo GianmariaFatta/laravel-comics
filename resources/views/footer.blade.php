91d883ad0869dc08128a1ecbc86b5d6a9a2d95f3
GianmariaFatta <gianmaria.fatta@gmail.com>
2023-02-23 19:11:54 +0100

    a lot of erors

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+     <footer>
+        <div class="container">
+
+            <div class="row">
+                <div class="s-footer">
+                    <button>
+                        <a href="">SIGN-UP NOW!</a>
+                    </button>
+                </div>
+                <div class="e-footer">
+                    <h3>FOLLOW US</h3>
+                    <div class="socials">
+                        <a href=""><img src="{{asset('img/footer-facebook.png')}}" alt=""></a>
+                        <a href=""><img src="{{asset('img/footer-twitter.png')}}" alt=""></a>
+                        <a href=""><img src="{{asset('img/footer-youtube.png')}}" alt=""></a>
+                        <a href=""><img src="{{asset('img/footer-pinterest.png')}}" alt=""></a>
+                        <a href=""><img src="{{asset('img/footer-periscope.png')}}" alt=""></a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </footer>
+</body>
+</html>
